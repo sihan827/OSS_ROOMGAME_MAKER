be2a6b81a8dd7dae99a8ec60e8e868adc2ea65e4
SangwooPark <sw0082@naver.com>
2020-05-22 23:25:37 +0900

    test2

--- RoomGameMaker/src/application/Main.java
+++ RoomGameMaker/src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-			System.out.println("fuck");
+			System.out.println("fuc");
 		}
 	}
 	
